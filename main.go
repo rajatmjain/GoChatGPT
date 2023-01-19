1daa176950da0ab61365d75af373800d976c7e38
rjain032 <rajat.jain@email.ucr.edu>
2023-01-19 16:20:48 +0530

    ChatGPT interactor

--- main.go
+++ main.go
@@ -1,1 +1,63 @@
 package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	gpt3 "github.com/PullRequestInc/go-gpt3"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func main(){
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+	apiKey := viper.GetString("API_KEY")
+	if apiKey==""{
+		log.Fatal("Empty API_KEY")
+	}
+
+	ctx := context.Background()
+	client := gpt3.NewClient(apiKey)
+	rootCmd := &cobra.Command{
+		Use: "chatgpt",
+		Short: "Chat with ChatGPT console.",
+		Run: func(cmd *cobra.Command, args []string) {
+			scanner := bufio.NewScanner(os.Stdin)
+			quit := false
+
+			for !quit{
+				fmt.Print("\nSay something or say 'quit' to end: ")
+				if !scanner.Scan(){
+					break
+				}
+				question := scanner.Text()
+				switch question{
+				case "quit":
+					quit = true
+				default:
+					GetResponse(client, ctx, question)
+				}	
+			}
+		},
+	}
+	rootCmd.Execute()
+}
+
+func GetResponse(client gpt3.Client,ctx context.Context, question string){
+	err := client.CompletionStreamWithEngine(ctx,gpt3.TextDavinci003Engine,gpt3.CompletionRequest{
+		Prompt: []string{question,},
+		MaxTokens: gpt3.IntPtr(3000),
+		Temperature: gpt3.Float32Ptr(0),
+	},func(cr *gpt3.CompletionResponse) {
+		fmt.Print(cr.Choices[0].Text)
+	})
+	if err!=nil{
+		fmt.Println(err)
+		os.Exit(13)
+	}
+	fmt.Println()
+}
